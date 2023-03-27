c6b1fa9dfb7d548bd45214ac80da75ddb7bf69a7
Aditya Yadav <Adityayadav44556@gmail.com>
2023-03-27 22:34:07 +0530

    Basic of Java file is added

---
+++ Basics of Java/src/HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// public (1st line) - It is an access modifier which allow to access class from anywhere
+// class (1st line) - It is the name group of properties and functions.
+// HelloWorld(1st line) - It is the name of the class as well as the name of the file.
+// main (2nd line) - 'main' is the name of the function/method. main function is the collection of code,
+// and it is the function from where program begins.
+// public (2nd line) - It allow the program to use main function/method from anywhere.
+// static (2nd line) - static is a keyword which allow to run main function without using object.
+// void (2nd line) - void is a keyword which is used when we don't want to return anything from the main function.
+// String[] args (2nd line) - It is a command line argument of string type array.
+
+
+// System (3rd line) - It is the final class defined in java.lang package.
+//out (3rd line) - It is the variable of PrintStream type
+// println ( 3rd line ) - It is the function of PrintStream class. It prints the argument passed to it and adds a new line.
+
+
+
+
