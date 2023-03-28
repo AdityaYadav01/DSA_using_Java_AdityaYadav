68d7bd5fddf36d5d7619c17e34c7e0d2d9b101c2
Aditya Yadav <Adityayadav44556@gmail.com>
2023-03-28 14:04:17 +0530

    Primitive Datatypes file is added

---
+++ Basics of Java/src/PrimitiveDatatype.java
@@ -0,0 +1,20 @@
+// Primitive datatypes are those datatypes which are not breakable.
+//Therefore, String is not a primitive datatype.
+
+public class PrimitiveDatatype {
+    public static void main(String[] args){
+        int num = 45; // int is a datatype which is used to store integer value.
+        float marks = 89.45f; // float is used to store decimal value.
+        char ch = 'A'; //char is used to store character.
+        double largedecimalvalue = 8956.2356515; // It is used to store large decimal values.
+        long l = 8744446659596154L; // It is used to store large integer values.
+        boolean ab = true; // Only store True and False
+
+        System.out.println(num);
+        System.out.println(marks);
+        System.out.println(ch);
+        System.out.println(largedecimalvalue);
+        System.out.println(l);
+        System.out.println(ab);
+    }
+}
