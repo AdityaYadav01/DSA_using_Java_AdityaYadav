e54eabc4a4c008a9c4bff37e69d45a32f158b95f
Aditya Yadav <Adityayadav44556@gmail.com>
2023-04-03 19:17:33 +0530

    Pass by value for Swapping of two numbers.

---
+++ MethodsOrFunctions/src/PassByValue.java
@@ -0,0 +1,17 @@
+public class PassByValue {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;                  // Swaping will not take place because both the numbers are swapped in other function.
+
+        swap(n1, n2);
+        System.out.println(n1 + " " + n2);
+    }
+
+    static void swap(int s1, int s2) {        // Pass by value
+        int temp = s1;
+        s1 = s2;
+        s2 = temp;
+    }
+
+
+}
